7c81b8a75425fc0d09b0a9138dc3c5bd6db813f0
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2021-10-21 11:03:54 -0700

    Update BoundingBox2.cs

--- Kinematica_Demo_YOLOX/Assets/OpenVINO/Scripts/BoundingBox2.cs
+++ Kinematica_Demo_YOLOX/Assets/OpenVINO/Scripts/BoundingBox2.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class BoundingBox2
 {
-    // Contains the bounding box
-    private GameObject bboxLine1 = new GameObject();
-    private GameObject bboxLine2 = new GameObject();
-    private GameObject bboxLine3 = new GameObject();
-    private GameObject bboxLine4 = new GameObject();
     // Contains the label text
     private GameObject text = new GameObject();
     // The canvas on which the bounding box labels will be drawn
@@ -21,19 +17,14 @@ public class BoundingBox2
     private Color color;
 
     // The adjusted line width for the bounding box
-    private int lineWidth = (int)(Screen.width * 1.75e-3);
-    private int lineThickness = 6;
+    private int lineWidth = 6;
     // The adjusted font size based on the screen size
     private float fontSize = (float)(Screen.width * 1.5e-2);
 
     // The label text
     private TextMeshProUGUI textContent;
 
-    // The bounding box lines
-    private Image bboxImage1;
-    private Image bboxImage2;
-    private Image bboxImage3;
-    private Image bboxImage4;
+    private GameObject[] bboxLines = new GameObject[4];
 
 
     /// <summary>
@@ -66,10 +57,11 @@ public class BoundingBox2
     /// <param name="show"></param>
     public void ToggleBBox(bool show)
     {
-        bboxLine1.SetActive(show);
-        bboxLine2.SetActive(show);
-        bboxLine3.SetActive(show);
-        bboxLine4.SetActive(show);
+        for (int i = 0; i < bboxLines.Length; i++)
+        {
+            bboxLines[i].SetActive(show);
+        }
+        
         text.SetActive(show);
     }
 
@@ -85,38 +77,37 @@ public class BoundingBox2
         float height = info.height;
 
         // Offset value to align the bounding box points
-        float offset = lineThickness / 2;
+        float offset = lineWidth / 2;
+
+
+        Vector2[] dimensions = new Vector2[]
+        {
+            new Vector2(width + lineWidth, lineWidth),
+            new Vector2(lineWidth, height + lineWidth),
+            new Vector2(width + lineWidth, lineWidth),
+            new Vector2(lineWidth, height + lineWidth)
+        };
+
+
+        Vector3[] positions = new Vector3[] 
+        {
+            new Vector3(x0 + (width/2), y0, 0),
+            new Vector3(x0 + width, y0 - (height/2), 0),
+            new Vector3(x0 + (width / 2), (y0 - height), 0),
+            new Vector3(x0, y0 - (height / 2), 0)
+        };
 
         // Set the material color
-        bboxImage1.color = color;
-        bboxImage2.color = color;
-        bboxImage3.color = color;
-        bboxImage4.color = color;
+        for (int i = 0; i < bboxLines.Length; i++)
+        {
+            Image bboxLine = bboxLines[i].GetComponent<Image>();
+            bboxLine.color = color;
+            RectTransform transform = bboxLine.rectTransform;
+            transform.sizeDelta = dimensions[i];
+            transform.position = positions[i];
 
-        // Top Line
-        RectTransform rectTransform = bboxImage1.GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(width + lineThickness, lineThickness);
-        Vector3 pos0 = new Vector3(x0 + (width/2), y0, 0);
-        rectTransform.position = pos0;
-
-        // Right Line
-        rectTransform = bboxImage2.GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(lineThickness, height + lineThickness);
-        Vector3 pos1 = new Vector3(x0 + width, y0 - (height/2), 0);
-        rectTransform.position = pos1;
-
-        // Bottom Line
-        rectTransform = bboxImage3.GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(width + lineThickness, lineThickness);
-        Vector3 pos2 = new Vector3(x0 + (width / 2), (y0 - height), 0);
-        rectTransform.position = pos2;
-
-        // Left Line
-        rectTransform = bboxImage4.GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(lineThickness, height + lineThickness);
-        Vector3 pos3 = new Vector3(x0, y0 - (height / 2), 0);
-        rectTransform.position = pos3;
-
+        }
+        
         // Make sure the bounding box is visible
         ToggleBBox(true);
     }
@@ -131,10 +122,11 @@ public class BoundingBox2
         info = objectInfo;
         // Get the object class label
         string name = Utils.object_classes[objectInfo.label].Item1;
-        bboxLine1.name = name + "_line_1";
-        bboxLine2.name = name + "_line_2";
-        bboxLine3.name = name + "_line_3";
-        bboxLine4.name = name + "_line_4";
+        for (int i = 0; i < bboxLines.Length; i++)
+        {
+            bboxLines[i].name = $"{name}+_line_{i}";
+        }
+        
         text.name = name;
         // Get the object class color
         color = Utils.object_classes[objectInfo.label].Item2;
@@ -156,16 +148,13 @@ public class BoundingBox2
         // Assign text object to the label canvas
         text.transform.SetParent(canvas.transform);
 
-        bboxLine1.transform.SetParent(canvas.transform);
-        bboxLine2.transform.SetParent(canvas.transform);
-        bboxLine3.transform.SetParent(canvas.transform);
-        bboxLine4.transform.SetParent(canvas.transform);
+        for (int i=0; i < bboxLines.Length; i++)
+        {
+            bboxLines[i] = new GameObject();
+            bboxLines[i].AddComponent<Image>();
+            bboxLines[i].transform.SetParent(canvas.transform);
+        }
 
-        bboxImage1 = bboxLine1.AddComponent<Image>();
-        bboxImage2 = bboxLine2.AddComponent<Image>();
-        bboxImage3 = bboxLine3.AddComponent<Image>();
-        bboxImage4 = bboxLine4.AddComponent<Image>();
-        
         // Update the object info for the bounding box
         SetObjectInfo(objectInfo);
     }
